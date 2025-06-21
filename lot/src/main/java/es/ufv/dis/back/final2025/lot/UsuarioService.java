5cbaa7ffbb16d8332109bda7a53c8946b209e1c7
Lara <laradelao2004@gmail.com>
2025-06-21 19:36:51 +0200

    Creacion e implementacion de UsuarioService.java

---
+++ lot/src/main/java/es/ufv/dis/back/final2025/lot/UsuarioService.java
@@ -0,0 +1,35 @@
+package es.ufv.dis.back.final2025.lot;
+
+import es.ufv.dis.back.final2025.lot.Usuario;
+import es.ufv.dis.back.final2025.lot.LectorJSON;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Service
+public class UsuarioService {
+
+    private List<Usuario> usuarios;
+
+    public UsuarioService() {
+        // Creamos una instancia de LectorJSON y leemos los usuarios
+        LectorJSON lector = new LectorJSON();
+        this.usuarios = lector.leeFicheroJson();
+    }
+
+    public List<Usuario> findAll() {
+        return usuarios;
+    }
+
+    public Usuario findById(String id) {
+        return usuarios.stream()
+                .filter(u -> u.getId().toString().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public void delete(String id) {
+        usuarios.removeIf(u -> u.getId().toString().equals(id));
+    }
+}
