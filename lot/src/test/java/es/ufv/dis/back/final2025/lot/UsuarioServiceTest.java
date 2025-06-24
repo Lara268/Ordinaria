5a5a5e6b14de9ad1a239e6a86d4772050a10ba89
Lara <laradelao2004@gmail.com>
2025-06-24 20:15:23 +0200

    Clase UsuarioServiceTest.java creada

---
+++ lot/src/test/java/es/ufv/dis/back/final2025/lot/UsuarioServiceTest.java
@@ -0,0 +1,30 @@
+package es.ufv.dis.back.final2025.lot;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioServiceTest {
+
+    @BeforeEach
+    void setUp() {
+        servicio = new UsuarioService();
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void actualizarUsuario() {
+    }
+
+    @Test
+    void crearUsuario() {
+    }
+}
