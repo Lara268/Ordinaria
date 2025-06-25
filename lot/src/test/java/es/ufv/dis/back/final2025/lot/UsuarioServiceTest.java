6ad1486956e8095ad644ecb574dca31d681c1c49
Lara <laradelao2004@gmail.com>
2025-06-25 16:37:06 +0200

    Test de buscar todos los usuarios

--- lot/src/test/java/es/ufv/dis/back/final2025/lot/UsuarioServiceTest.java
+++ lot/src/test/java/es/ufv/dis/back/final2025/lot/UsuarioServiceTest.java
@@ -3,9 +3,12 @@ package es.ufv.dis.back.final2025.lot;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UsuarioServiceTest {
+    private UsuarioService servicio;
 
     @BeforeEach
     void setUp() {
@@ -14,6 +17,9 @@ class UsuarioServiceTest {
 
     @Test
     void findAll() {
+        List<Usuario> usuarios = servicio.findAll();
+        assertNotNull(usuarios);
+        assertFalse(usuarios.isEmpty());
     }
 
     @Test
