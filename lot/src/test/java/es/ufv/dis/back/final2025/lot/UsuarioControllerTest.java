6a339a5221400d279bd51947619f5ca149fc08bb
Lara <laradelao2004@gmail.com>
2025-06-25 20:34:59 +0200

    Creacion del test GetUsuarios

--- lot/src/test/java/es/ufv/dis/back/final2025/lot/UsuarioControllerTest.java
+++ lot/src/test/java/es/ufv/dis/back/final2025/lot/UsuarioControllerTest.java
@@ -1,17 +1,28 @@
 package es.ufv.dis.back.final2025.lot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UsuarioControllerTest {
 
+    final String baseUrl = "http://localhost:8081/api/usuarios";
+    RestTemplate restTemplate = new RestTemplate();
+
     @Test
     void getUsuarios() {
+        ResponseEntity<Usuario[]> response = restTemplate.getForEntity(baseUrl, Usuario[].class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertTrue(response.getBody().length > 0);
     }
 
     @Test
     void getUsuario() {
+
     }
 
     @Test
