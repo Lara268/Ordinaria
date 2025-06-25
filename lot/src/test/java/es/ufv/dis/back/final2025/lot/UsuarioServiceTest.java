4fb5812397806368556e8f7f7e5dd77c3ef580c2
Lara <laradelao2004@gmail.com>
2025-06-25 16:43:50 +0200

    Test de actualizar un usuario

--- lot/src/test/java/es/ufv/dis/back/final2025/lot/UsuarioServiceTest.java
+++ lot/src/test/java/es/ufv/dis/back/final2025/lot/UsuarioServiceTest.java
@@ -2,6 +2,7 @@ package es.ufv.dis.back.final2025.lot;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.UUID;
@@ -33,6 +34,17 @@ class UsuarioServiceTest {
 
     @Test
     void actualizarUsuario() {
+        Usuario original = servicio.findAll().get(0);
+        Usuario modificado = new Usuario();
+        modificado.setId(original.getId());
+        modificado.setNombre("NuevoNombre");
+        modificado.setApellidos(original.getApellidos());
+        modificado.setNif(original.getNif());
+        modificado.setEmail(original.getEmail());
+
+        ResponseEntity<Usuario> respuesta = servicio.actualizarUsuario(original.getId(), modificado);
+        assertEquals("NuevoNombre", servicio.findById(original.getId()).getNombre());
+        assertEquals(200, respuesta.getStatusCodeValue());
     }
 
     @Test
