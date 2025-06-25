f0014c831503a2d07d951b62d7bd49bffb4b7e1c
Lara <laradelao2004@gmail.com>
2025-06-25 20:46:36 +0200

    Creacion de la clase UsuarioTest.java

---
+++ lot_Front/src/test/java/org/vaadin/example/UsuarioTest.java
@@ -0,0 +1,64 @@
+package org.vaadin.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getNombre() {
+    }
+
+    @Test
+    void setNombre() {
+    }
+
+    @Test
+    void getApellidos() {
+    }
+
+    @Test
+    void setApellidos() {
+    }
+
+    @Test
+    void getNif() {
+    }
+
+    @Test
+    void setNif() {
+    }
+
+    @Test
+    void getDireccion() {
+    }
+
+    @Test
+    void setDireccion() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void getMetodoPago() {
+    }
+
+    @Test
+    void setMetodoPago() {
+    }
+}
