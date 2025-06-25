233d501f8b38843edabae2756c0a649370d489f0
Lara <laradelao2004@gmail.com>
2025-06-25 20:32:46 +0200

    Creacion de la clase UsuarioController.java

---
+++ lot/src/test/java/es/ufv/dis/back/final2025/lot/UsuarioControllerTest.java
@@ -0,0 +1,24 @@
+package es.ufv.dis.back.final2025.lot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioControllerTest {
+
+    @Test
+    void getUsuarios() {
+    }
+
+    @Test
+    void getUsuario() {
+    }
+
+    @Test
+    void actualizarUsuario() {
+    }
+
+    @Test
+    void crearUsuario() {
+    }
+}
